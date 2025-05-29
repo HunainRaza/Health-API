19b5589e956f0f7974adfe59bf5570ea7b1308f4
Hunain Raza <hunain.a4@gmail.com>
2025-05-29 06:00:57 +0200

    switched-back-on-allauth

--- core/settings/common.py
+++ core/settings/common.py
@@ -34,6 +34,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Third party apps
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'rest_framework',
     'rest_framework.authtoken',
     'django_filters',
@@ -54,6 +57,7 @@ MIDDLEWARE = [
     'users.middleware.TokenExpiryMiddleware',  # Custom token expiry middleware
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "allauth.account.middleware.AccountMiddleware",
 ]
 
 ROOT_URLCONF = 'core.urls'
